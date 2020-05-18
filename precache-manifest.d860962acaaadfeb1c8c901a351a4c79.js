81c37ac3b271b3fc67718a1116eb76cd2ef663e3
praton90 <pablo.raton@goeuro.com>
2020-05-18 17:51:04 +0200

    Updates

---
+++ precache-manifest.d860962acaaadfeb1c8c901a351a4c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b75523c3db8fd31c9bfb64086e54c2",
+    "url": "/agileup-website/index.html"
+  },
+  {
+    "revision": "99a361047d6ed0bea78b",
+    "url": "/agileup-website/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "a53176b78fd3b0ed46eb",
+    "url": "/agileup-website/static/css/main.dffbd0ad.chunk.css"
+  },
+  {
+    "revision": "99a361047d6ed0bea78b",
+    "url": "/agileup-website/static/js/2.163790f7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/agileup-website/static/js/2.163790f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53176b78fd3b0ed46eb",
+    "url": "/agileup-website/static/js/main.291fff75.chunk.js"
+  },
+  {
+    "revision": "1d79c6f13d4c183ff343",
+    "url": "/agileup-website/static/js/runtime-main.e61d1c91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/agileup-website/precache-manifest.1dcadd1b90c9259ed4a3dea8995843a1.js"
+  "/agileup-website/precache-manifest.d860962acaaadfeb1c8c901a351a4c79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/agileup-website/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
