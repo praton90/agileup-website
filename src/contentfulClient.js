e5135c6ee6300a088a1e998683a2f08e903ba5c9
Pablo Raton <praton90@gmail.com>
2020-05-22 18:51:44 +0200

    export contentful client

---
+++ src/contentfulClient.js
@@ -0,0 +1,8 @@
+import * as contentful from "contentful";
+
+const contentfulClient =  contentful.createClient({
+    space: "5ewpe53pwxis",
+    accessToken: "Xerq7AgfY3obWybUXHTuhYnXSNUa5aHgd3l5SQnY-jk",
+});
+
+export default contentfulClient;
