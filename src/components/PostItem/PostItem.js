4f8b5a31d457261c5b1f289ef37d2e6a227aede7
Pablo Raton <praton90@gmail.com>
2020-05-24 16:32:53 +0200

    PostItem only with title

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -3,7 +3,7 @@ import React from "react";
 import Card from "../Aux/Card/Card";
 import { Link } from "react-router-dom";
 
-import { FaAt, FaCalendarAlt } from "react-icons/fa";
+// import { FaAt, FaCalendarAlt } from "react-icons/fa";
 
 import "./PostItem.css";
 
@@ -11,7 +11,7 @@ const postItem = (props) => {
   return (
     <Card>
       <h2 className="post__title">{props.post.title}</h2>
-      <div className="post__metadata">
+      {/* <div className="post__metadata">
         <span className="icon">
           <FaAt />
         </span>
@@ -23,7 +23,7 @@ const postItem = (props) => {
         </span>
         {new Date(props.post.createdAt).toDateString()}
       </div>
-      <p className="post__intro">{props.post.body}</p>
+      <p className="post__intro">{props.post.body}</p> */}
       <Link className="btn btn--right" to={"/posts/" + props.post.id}>
         Read More
       </Link>
